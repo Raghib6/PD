695902b2c77f7dba6118b4482aa308f0d7bb68d2
Raghib Shahriar <raghibshahriar6@gmail.com>
2021-03-01 16:48:09 +0600

    Just changed the range of two list

--- table.py
+++ table.py
@@ -7,8 +7,8 @@ i_time = []
 a_time = [0]
 print('\n')
 print('Customer No\t\tInterarrival Time\t\tArrival Time on Clock')
-for i in range(5):
-    i_time.append(randint(1,10))
+for i in range(20):
+    i_time.append(randint(1,21))
 for r in range(1,len(i_time)+1):
     if r==1:
         print(f'\t{r}\t\t\t0\t\t\t\t{a_time[r-1]}')
